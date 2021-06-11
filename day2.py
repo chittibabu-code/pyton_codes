f2910976fc247f84c0ba4cc063fb5daa6df47fc6
chittibabu-code <85699063+chittibabu-code@users.noreply.github.com>
2021-06-11 18:55:56 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,40 @@
+#Dtatypes
+
+#string
+name="chitti"
+print(name)
+print(type(name))
+
+#Int
+age=24
+print(age)
+print(type(age))
+
+#Float
+sal=2455.4
+print(sal)
+print(type(sal))
+
+#Boolean
+d=True
+print(d)
+print(type(d))
+
+
+#format
+
+name    = "Chitti" 
+age     =24
+sal     3050.0
+
+info="my Name is {}.i am {} years old and my salary is {}."format(name, age, sal)
+print(info)
+
+print()
+
+myInfo=f"my name is {name}.i am {age} years old and my salary is {sal}."
+
+print(myInfo)
+
+
+
