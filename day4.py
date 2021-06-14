f9707b8cb061c3a5b2e45818fde8c10ef9827c3a
chittibabu-code <85699063+chittibabu-code@users.noreply.github.com>
2021-06-14 10:21:38 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,23 @@
+
+#evencharacters
+st="Hello World"
+str2 = (st[::2])
+print (str2)
+
+#swapcase
+
+Str = "EdUyEaR"
+str2 = str.swapcase()
+print (str2)
+
+
+#count of character
+string = "Python Eduyear"
+substring = "y"
+
+count = string.count(substring)
+
+print(count)
+
+
+
